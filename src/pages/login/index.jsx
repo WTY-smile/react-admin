ddad5ad981540af9e5350b00afed22c3328f831d
WTY <2252440337@qq.com>
2019-06-22 01:32:36 +0800

    完成表单css样式

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,18 +1,22 @@
 import React, {Component} from 'react';
 
 import { Form, Icon, Input, Button } from 'antd';
+// 引入logo图片资源，在React脚手架中图片必须引入才会打包使用
 import logo from './logo.png';
+
+// import 语法必须在最上面，只要不是在最上面就会报错
+import './index.less';
 
 const Item = Form.Item;
 
 class Login extends Component{
+    // 阻止submit默认提交表单
     login = (e) => {
       e.preventDefault();
     };
 
     render() {
-        return (
-            <div>
+        return <div className="login">
                 <header className="login-header">
                     <img src={logo} alt="logo"/>
                     <h1>React项目: 后台管理系统</h1>
@@ -31,9 +35,7 @@ class Login extends Component{
                         </Item>
                     </Form>
                 </section>
-            </div>
-
-        )
+            </div>;
     }
 }
 
